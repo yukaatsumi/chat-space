ca4fff41f8c2a834a168989cd9904d74ab5f74f0
渥美佑佳 <imusta99ym@gmail.com>
2018-04-02 16:23:27 +0900

    Fix_incremental_search_function

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -18,15 +18,15 @@ var search_list = $("#user-search-result");
   function appendNoUser(user){
     var html =
               `<div class="chat-group-user clearfix">
-                <p class="chat-group-user__name">${name}</p>
+                <p class="chat-group-user__name">${user}</p>
               </div>`
       search_list.append(html);
   }
 
 // ユーザー追加ボタンのクリックで追加されるHTMLの生成
-function buildMenberHTML(id, name){
+function buildMemberHTML(id, name){
   var html =
-            `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+            `<div class='chat-group-user clearfix js-chat-member'>
               <input name='group[user_ids][]' type='hidden' value='${id}'>
               <p class='chat-group-user__name'>${name}</p>
               <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
@@ -37,37 +37,37 @@ function buildMenberHTML(id, name){
 // インクリメンタルサーチ
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
-
-    $.ajax({
-      type: 'GET',
-      url:'/users',
-      data: { keyword: input },
-      dataType: 'json'
-    })
-// ユーザーの検索が成功した場合
-    .done(function(users) {
-      $("#user-search-result").empty();
-      if (users.length !== 0 && input.length !== 0) {
-        users.forEach(function(user){
-          appendUser(user);
-        });
-      } else {
-        appendNoUser("一致するユーザーはいません");
-      }
-    })
-// ユーザーの検索に失敗した場合
-    .fail(function() {
-      alert('ユーザーの検索に失敗しました');
-    });
+    if (input != "" ){
+      $.ajax({
+        type: 'GET',
+        url:'/users',
+        data: { keyword: input },
+        dataType: 'json'
+      })
+  // ユーザーの検索が成功した場合
+      .done(function(users) {
+        $("#user-search-result").empty();
+        if (users.length !== 0) {
+          users.forEach(function(user){
+            appendUser(user);
+          });
+        } else {
+          appendNoUser("一致するユーザーはいません");
+        }
+      })
+  // ユーザーの検索に失敗した場合
+      .fail(function() {
+        alert('ユーザーの検索に失敗しました');
+      });
+    }
   });
 
 // メンバーの追加
   $('#user-search-result').on('click', '.user-search-add', function(e) {
     e.preventDefault();
     var user_id = $(this).attr('data-user-id');
-    console.log(user_id);
     var user_name = $(this).attr('data-user-name');
-    buildMenberHTML(user_id, user_name);
+    buildMemberHTML(user_id, user_name);
   });
 // メンバーの削除
   $('#user-search-result').on("click", ".user-search-remove", function(){
